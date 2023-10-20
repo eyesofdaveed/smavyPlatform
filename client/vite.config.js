7f112b3daf01b7049a041881b7636647b49223f9
Aidyn <aidyn.meiramkhanuly@gmail.com>
2023-10-20 19:50:39 +0600

    added alias and server proxy vite.cfg

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,29 @@
 import { defineConfig } from 'vite'
+import path from 'path';
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@api': path.resolve(__dirname, './src/api'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@utils': path.resolve(__dirname, './src/utils'),
+      '@routes': path.resolve(__dirname, './src/routes'),
+      '@entities': path.resolve(__dirname, './src/entities'),
+      '@features': path.resolve(__dirname, './src/features'),
+    },
+  },
+  
+  server: {
+    open: true,
+    port: 3000,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8000/',
+        changeOrigin: true,
+      },
+    },
+  },
 })
