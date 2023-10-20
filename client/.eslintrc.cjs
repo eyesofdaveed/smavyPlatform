849bdf169e88efae154930524f6b5b9721db88b2
Aidyn <aidyn.meiramkhanuly@gmail.com>
2023-10-20 18:56:27 +0600

    fixed eslintr file

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -1,20 +1,54 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: { 
+    browser: true,
+     es2020: true,
+     node: true,
+ },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
+    'airbnb',
+    'airbnb/hooks',
+    'plugin:@tanstack/eslint-plugin-query/recommended',
+    'prettier',
+
   ],
+  overrides: [],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
+  parserOptions: { 
+    ecmaFeatures: { jsx: true },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+   },
+  settings: { 
+    react: { 
+      version: '18.2'
+   },
+    'import/resolver': {
+      node: {
+        paths: ['src'],
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
   plugins: ['react-refresh'],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+    indent: ['error', 2, { SwitchCase: 1, offsetTernaryExpressions: true }],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+    'no-promise-executor-return': 'off',
+    'no-restricted-imports': ['error', { patterns: ['@features/*/*'] }],
+    'no-bitwise': 'off',
+    'import/prefer-default-export': 'off',
+    'import/no-extraneous-dependencies': 'off',
+    '@typescript-eslint/no-unu  sed-vars': ['warn', { argsIgnorePattern: '^_' }],
+    'react/react-in-jsx-scope': 'off',
+    'react/function-component-definition': 'off',
+    'react/require-default-props': 'off',
+    'react/jsx-props-no-spreading': 'off',
   },
+}
 }
