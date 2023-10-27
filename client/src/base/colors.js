6c5e98701c7f45feda98932545cd6faf7dc19a59
jqmbylqyzy <dina.zhambylqyzy@jihc.kz>
2023-10-27 16:17:22 +0600

    added constant colors

--- client/src/base/colors.js
+++ client/src/base/colors.js
@@ -2,4 +2,12 @@ export const colors = {
   white: '#FFFFFF',
   black: '#000000',
   cardBg: '#737399',
+  btnPrimary: '#5BAFFC',
+  sidebarColor: '#EDEEF2',
+  eventRed: "#F9BDBD",
+  eventYellow: "#F5F7B7",
+  eventBlue: "#BEBDF9",
+  notificationColor: "#E4F3FB",
+  profileHeaderBG: "#3B4051",
+  test_taskCardBG: "#E5F5FC"
 };
