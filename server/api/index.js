47b20bdc06ac6dcfcaa773c27103b62f333f77a8
DonJuan <armvnov1ch@gmail.com>
2023-11-01 10:08:06 +0600

    added findOne, deleteOne, UpdateById, deleteAll

--- server/api/index.js
+++ server/api/index.js
@@ -12,7 +12,18 @@ class Entity {
         }
     }
 
-    async get() {}
+    async get(res) {
+        try {
+            let id = req.params.id;
+            let entity = await this.entityModel.findByPk(id);
+            if (data)
+                return res.status(200).json({ entity })
+            else
+                return res.status(400).json({ success: false, error: "No such user present", data: [] });
+            } catch (err) {
+                console.log(err);
+            }
+        }
 
     async getAll(res) {
         try {
@@ -23,11 +34,37 @@ class Entity {
         }
     }
 
-    async update() {}
+    async update(res) {
+        try {
+            const entities = await this.entityModel.findByIdAndUpdate(req.params.id, {
+            $set: req.body,
+            });
+            res.status(200).json(entities);
+        } catch (err) {
+            return res.status(400).json({ message: err.errors });
+        }
+    }
 
-    async delete() {}
+    async delete(res) {
+        const deletionCriteria = { _id: 123 };
+        try {
+            const entities = await this.entityModel.deleteOne({deletionCriteria
+            });
+            res.status(200).json(entities);
+        } catch (err) {
+            console.log(err);
+        }
+    }
+    
 
-    async deleteAll() {}
-}
+    async deleteAll(res) {
+        try {
+            const data = await this.entityModel.destroy({where: {}, truncate: true});
+        return res.status(200).json({ success: true, data: data });
+        } catch (err) {
+            console.log(err);
+        }
+    }
+};
 
 module.exports = Entity;
