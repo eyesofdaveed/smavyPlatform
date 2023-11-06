55bda89870dfe903e999aa9d7c6eb0f75cbee113
eyesofdaveed <davdauit@gmail.com>
2023-11-06 17:54:26 +0600

    add contracts

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,16 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom';
 
 import Login from './pages/Login';
 import NotFoundPage from './pages/404/404';
-
 
 const App = () => {
   return (
     <>
       <Routes>
-        <Route path='*' element={<NotFoundPage />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/' element={<Login />} />
+        <Route path="*" element={<NotFoundPage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Login />} />
       </Routes>
     </>
   );
