9659255eaa0f5691480ef6d9bc701b151c9c24df
akenaaa <aruzhan.tolegen@jihc.kz>
2023-11-10 14:39:26 +0600

    FE-changes-personal-data-card

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,7 +11,7 @@ const App = () => {
         <Route path="*" element={<NotFoundPage />} />
         <Route path="/login" element={<Login />} />
         <Route path="/" element={<Login />} />
-        </Routes>
+      </Routes>
     </>
   );
 };
