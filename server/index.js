d03a32e8f9390ad0e458420a5bf5fabdf3dc3038
Daulet Zhanysbay <daulet.zhanysbay@gmail.com>
2023-11-11 14:37:42 +0600

    add swagger

--- server/index.js
+++ server/index.js
@@ -14,6 +14,23 @@ const logoutRoute = require('./routes/logout');
 const registerRoute = require('./routes/register');
 const { logger, logEvents } = require('./middleware/logger');
 const verifyJwt = require('./middleware/verifyJwt');
+const swaggerJsdoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Your API',
+      version: '1.0.0',
+    },
+  },
+  apis: ['routes/*.js'], // Укажите путь к вашим файлам маршрутов
+};
+
+const specs = swaggerJsdoc(options);
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
 
 dotenv.config();
 
