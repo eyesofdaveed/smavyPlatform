f231ea029fc1749c6bb828201ab5a78134816a2f
asseemm <assem.shimbergenova@jihc.kz>
2023-11-14 09:21:49 +0600

    fix problems in text atom

--- client/src/atoms/Text.jsx
+++ client/src/atoms/Text.jsx
@@ -2,14 +2,6 @@ import React from 'react';
 import styled from 'styled-components';
 
 export const TextContainer = styled.div`
-  font-weight: ${({ fontWeight }) => (fontWeight ? fontWeight : '500')};
-  line-height: ${({ lineHeight }) => (lineHeight ? lineHeight : '32px')};
-  font-size: ${({ fontSize }) => (fontSize ? fontSize : '16px')};
-  color: ${({ color }) => (color ? color : '#fff')};
-`;
-
-export const Text = ({ fontSize, children, color, fontWeight, lineHeight }) => {
-  return <TextContainer fontSize={fontSize} color={color}  fontWeight={fontWeight} lineHeight={lineHeight} >{children}</TextContainer>;
   text-align: ${({ textAlign }) => (textAlign ? textAlign : 'center')};
   font-weight: ${({ fontWeight }) => (fontWeight ? fontWeight : '500')};
   line-height: ${({ lineHeight }) => (lineHeight ? lineHeight : '26px')};
