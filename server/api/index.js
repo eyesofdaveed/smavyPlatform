36da64200c648fea09449555571f80a713010b45
Darkhan Zhursin <daratechn@gmail.com>
2023-11-17 12:22:17 +0600

    fix: replaced with error handler and lodash

--- server/api/index.js
+++ server/api/index.js
@@ -1,3 +1,5 @@
+const _ = require('lodash');
+
 const { isEmptyObject } = require('../utils');
 const errorHandler = require('../middleware/errorHandler');
 
@@ -17,18 +19,23 @@ class Entity {
 
   async getById(req, res, entityName) {
     try {
-      if (!req && !req.params && !req.params.id) {
-        return res.status(400).json({ message: `${entityName} ID required.` });
+      const entityId = _.get(req, 'params.id');
+      if (!entityId) {
+        return errorHandler({
+          statusCode: 400,
+          message: `${entityName} ID required.`,
+        });
       }
 
       const requestedEntity = await this.entityModel
-        .findOne({ _id: req.params.id })
+        .findOne({ _id: entityId })
         .exec();
 
       if (!requestedEntity) {
-        return res
-          .status(400)
-          .json({ message: `${entityName} ID ${req.params.id} not found` });
+        return errorHandler({
+          statusCode: 400,
+          message: `${entityName} ID ${entityId} not found`,
+        });
       }
 
       res.json(requestedEntity);
@@ -37,10 +44,10 @@ class Entity {
     }
   }
 
-  async getAll({ req, res, pageSize = '25', pageNumber = '1' }) {
+  async getAll({ req, res }) {
     try {
-      const pageSizeInt = parseInt(pageSize);
-      const pageNumberInt = parseInt(pageNumber);
+      const pageSizeInt = parseInt(_.get(req, 'body.filter', '25'));
+      const pageNumberInt = parseInt(_.get(req, 'body.filter', '1'));
 
       const results = await this.entityModel
         .find()
@@ -48,6 +55,7 @@ class Entity {
           createdAt: -1,
         })
         .skip(pageSizeInt * (pageNumberInt - 1))
+
         .limit(pageSizeInt);
       return res.json({ data: results });
     } catch (err) {
@@ -64,13 +72,18 @@ class Entity {
       });
       res.status(200).json(entities);
     } catch (err) {
-      return res.status(400).json({ message: err.errors });
+      return errorHandler({
+        statusCode: 400,
+        message: err.errors,
+      });
+      //res.status(400).json({ message: err.errors });
     }
   }
 
   async delete(req, res, entityName) {
     try {
-      if (!req && !req.params && !req.params.id) {
+      const entityId = _.get(req, 'params.id');
+      if (!entityId) {
         return res.status(400).json({ message: `${entityName} ID required.` });
       }
 
@@ -79,9 +92,10 @@ class Entity {
         .exec();
 
       if (!requestedEntity) {
-        return res
-          .status(400)
-          .json({ message: `${entityName} ID ${req.body.id} not found` });
+        return errorHandler({
+          statusCode: 400,
+          message: `${entityName} ID ${req.body.id} not found`,
+        });
       }
 
       const result = await this.entityModel
