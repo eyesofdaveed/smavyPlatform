df6d296df3ec64aee76ee5069f7719b6a4b89e03
bashkanitto <aidyn.meiramkhanuly@gmail.com>
2023-11-17 10:29:33 +0600

    fixed conflict

--- client/src/atoms/Text.jsx
+++ client/src/atoms/Text.jsx
@@ -4,18 +4,6 @@ import styled from 'styled-components';
 
 export const TextContainer = styled.div`
   text-align: ${({textAlign }) => textAlign};
-<<<<<<< HEAD
-  font-weight: ${({ weight }) => (weight ? weight : '400')};
-  line-height: 32px;
-  font-size: ${({ fontSize }) => (fontSize ? fontSize : '16px')};
-  color: #fff;
-  text-align: ${({ textAlign }) => (textAlign ? textAlign : 'center')};
-`;
-
-export const Text = ({ fontSize, children, fontSizePhone, fontWeight, lineHeight, lineHeightPhone , color }) => {
-  return <TextContainer fontSize={fontSize} fontSizePhone={fontSizePhone} color={color} fontWeight={fontWeight} lineHeight={lineHeight} lineHeightPhone={lineHeightPhone}>{children}</TextContainer>;
-};
-=======
   font-weight: ${({ fontWeight }) => (fontWeight ? fontWeight : '500')};
   line-height: ${({ lineHeight }) => (lineHeight ? lineHeight : '26px')};
   font-size: ${({ fontSize }) => (fontSize ? fontSize : '16px')};
@@ -27,7 +15,8 @@ export const Text = ({ fontSize, children, fontSizePhone, fontWeight, lineHeight
   }
 `;
 
-export const Text = ({ fontSize, children, fontSizePhone, fontWeight, lineHeight, lineHeightPhone, color }) => {
-  return <TextContainer fontSize={fontSize} color={color} fontSizePhone={fontSizePhone} fontWeight={fontWeight} lineHeight={lineHeight} lineHeightPhone={lineHeightPhone}>{children}</TextContainer>;
+export const Text = ({ fontSize, children, fontSizePhone, fontWeight, lineHeight, lineHeightPhone , color }) => {
+  return <TextContainer fontSize={fontSize} fontSizePhone={fontSizePhone} color={color} fontWeight={fontWeight} lineHeight={lineHeight} lineHeightPhone={lineHeightPhone}>{children}</TextContainer>;
 };
->>>>>>> 23f6ecd7f34535050de93855662cd9180de87167
+
+
