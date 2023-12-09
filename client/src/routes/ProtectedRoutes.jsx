92b9e9e20fecff5af4d5a1b2f2839a58334edb14
asseemm <assem.shimbergenova@jihc.kz>
2023-12-09 14:48:04 +0600

    current page

--- client/src/routes/ProtectedRoutes.jsx
+++ client/src/routes/ProtectedRoutes.jsx
@@ -3,15 +3,16 @@ import styled from 'styled-components';
 import { Route, Routes } from 'react-router-dom';
 
 import MainLayout from '../MainLayout';
+import NotFoundPage from '@pages/NotFoundPage';
 import ProfilePage from '@pages/ProfilePage';
 import GradeBookPage from '@pages/GradeBookPage';
 import DisciplinesPage1 from '@pages/DisciplinesPage1';
-
 
 export const Protected = () => {
     return (
         <MainLayout>
             <Routes>
+                <Route path='*' element={<NotFoundPage />} />
                 <Route path='/profile' element={<ProfilePage />} />
                 <Route path='/gradebook' element={<GradeBookPage />} />
                 <Route path='/disciplines' element={<DisciplinesPage1 />} />
