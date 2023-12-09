4ffe8b132f805580bab8f308b6ae3ab974047456
akenaaa <aruzhan.tolegen@jihc.kz>
2023-12-09 14:03:54 +0600

    FE-personaldata-word

--- client/src/atoms/Text.jsx
+++ client/src/atoms/Text.jsx
@@ -6,10 +6,10 @@ export const TextContainer = styled.div`
 line-height: 32px;
 color: #fff;
 font-weight: ${({ weight }) => (weight ? weight : '300')};
-  font-size: ${({ fontSize }) => (fontSize ? fontSize : '16px')};
-  text-align: ${({ textAlign }) => (textAlign ? textAlign : 'center')};
+font-size: ${({ fontSize }) => (fontSize ? fontSize : '16px')};
+text-align: ${({ textAlign }) => (textAlign ? textAlign : 'center')};
 `;
 
-export const Text = ({ fontSize, children, fontSizePhone, fontWeight, lineHeight, lineHeightPhone , color }) => {
-  return <TextContainer fontSize={fontSize} fontSizePhone={fontSizePhone} color={color} fontWeight={fontWeight} lineHeight={lineHeight} lineHeightPhone={lineHeightPhone}>{children}</TextContainer>;
+export const Text = ({ fontSize, children, textAlign, fontSizePhone, fontWeight, lineHeight, lineHeightPhone , color }) => {
+  return <TextContainer fontSize={fontSize} textAlign={textAlign} fontSizePhone={fontSizePhone} color={color} fontWeight={fontWeight} lineHeight={lineHeight} lineHeightPhone={lineHeightPhone}>{children}</TextContainer>;
 };
--- client/src/organism/PersonalData.jsx
+++ client/src/organism/PersonalData.jsx
@@ -7,31 +7,55 @@ import { Card } from '@atoms';
 import { sizes,colors } from '@base';
 import { ValueBackround } from '../atoms';
 
+const PersonalDataContainer = styled.div`
+padding: ${({ padding }) => padding };
+margin : ${({ margin }) => margin } ;
+align-items: ${({align}) => align };
+
+
+@media (max-width: 600px) {
+  width: 100%;
+  margin: ${({ margin }) => (margin ? '10px 0' : '10px 0')};
+  padding: ${({ padding }) => (padding ? '2px 0px 5px 10px' : '2px 0px 5px 10px')}; 
+
+`;
+  
+export const personalData = ({ margin , padding , direction , align}) => {
+return < PersonalDataContainer margin={margin} padding={padding} direction={direction} align={align}></PersonalDataContainer>;
+};
+
 export const PersonalData = () => {
   return (
-    <Card padding="30px" display="flex" width="fit-content" bgColor={colors.cardMainBg} >
-      <Flexbox direction="column" gap="22px" align="start">
-        <Flexbox justify="space-between" width="100%">
-          <Text  color={colors.black} fontSize={sizes.medium} >ИИН</Text>
-          <ValueBackround margin="0px 3px 0px 10px" padding="2px 15px ">
-            <Text  color={colors.black} fontSize={sizes.medium} >010722547645</Text>
-          </ValueBackround>
+
+      <PersonalDataContainer> 
+        <PersonalDataContainer margin= {'30px'} > 
+        <Text  color={colors.black} fontSize={sizes.large} textAlign={'left'} >Персональные сведения</Text>
+        </PersonalDataContainer>
+      <Card padding="30px" display="flex" width="fit-content" bgColor={colors.cardMainBg} >
+        <Flexbox direction="column" gap="22px" align="start">
+          <Flexbox justify="space-between" width="100%">
+            <Text  color={colors.black} fontSize={sizes.medium} >ИИН</Text>
+            <ValueBackround margin="0px 3px 0px 10px" padding="2px 15px ">
+              <Text  color={colors.black} fontSize={sizes.medium} >010722547645</Text>
+            </ValueBackround>
+          </Flexbox>
+          <Flexbox justify="space-between" width="100%">
+            <Text  color={colors.black} fontSize={sizes.medium} >
+              Дата рождения
+            </Text>
+            <ValueBackround margin="0px 3px 0px 10px" padding="2px 15px ">
+              <Text color={colors.black} fontSize={sizes.medium}>22-07-2001</Text>
+            </ValueBackround >
+          </Flexbox>
+          <Flexbox justify="space-between" width="100%">
+            <Text  color={colors.black} fontSize={sizes.medium} >Пол</Text>
+            <ValueBackround margin="0px 3px 0px 10px" padding="2px 15px ">
+              <Text  color={colors.black} fontSize={sizes.medium}>Женский</Text>
+            </ValueBackround>
+          </Flexbox>
         </Flexbox>
-        <Flexbox justify="space-between" width="100%">
-          <Text  color={colors.black} fontSize={sizes.medium} >
-            Дата рождения
-          </Text>
-          <ValueBackround margin="0px 3px 0px 10px" padding="2px 15px ">
-            <Text color={colors.black} fontSize={sizes.medium}>22-07-2001</Text>
-          </ValueBackround >
-        </Flexbox>
-        <Flexbox justify="space-between" width="100%">
-          <Text  color={colors.black} fontSize={sizes.medium} >Пол</Text>
-          <ValueBackround margin="0px 3px 0px 10px" padding="2px 15px ">
-            <Text  color={colors.black} fontSize={sizes.medium}>Женский</Text>
-          </ValueBackround>
-        </Flexbox>
-      </Flexbox>
-    </Card>
+      </Card>
+   
+     </PersonalDataContainer>
   );
 };
