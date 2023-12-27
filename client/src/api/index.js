6ca9151037c9f748ae800bd74ad928748ddaa87a
bashkanitto <aidyn.meiramkhanulu@gmail.com>
2023-12-27 20:38:14 +0600

    added register and logo

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,12 +1,23 @@
 import axios from 'axios';
 
-const BASE_URL = 'https://smavybe-production.up.railway.app/';
+const baseUrl =
+  import.meta.env.VITE_ISPROD === 'false'
+    ? import.meta.env.VITE_PROD_URL
+    : import.meta.env.VITE_LOCAL_URL;
 
-export const baseApi = async (link, method) => {
+export const baseApi = async (link, method, data) => {
   switch (method) {
     case 'GET':
       try {
-        const response = await axios.get(`${BASE_URL}/${link}/`);
+        const response = await axios.get(`${baseUrl}/${link}/`);
+        console.log(response.data);
+        return response.data;
+      } catch (err) {
+        console.log(err);
+      }
+    case 'POST':
+      try {
+        const response = await axios.post(`${baseUrl}/${link}/`, data);
         return response.data;
       } catch (err) {
         console.log(err);
