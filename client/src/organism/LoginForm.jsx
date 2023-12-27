ee6d797961b2e2dca18186a23bf321bd93172fc3
bashkanitto <aidyn.meiramkhanulu@gmail.com>
2023-12-27 20:38:38 +0600

    refactor-loginForm

--- client/src/organism/LoginForm.jsx
+++ client/src/organism/LoginForm.jsx
@@ -1,71 +1,49 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-import { Card } from '@atoms';
-import { Flexbox } from '@atoms';
-import { Input } from '@atoms';
-import { Text } from '@atoms';
-import { sizes } from '@base/index';
 import { baseApi } from '@api';
 import { API_METHODS } from '@api/enums';
+import { Card, Flexbox, Input, Text } from '@atoms';
+import { sizes } from '@base/index';
 import { Button } from '../atoms/Button';
 
-const INPUT_TYPES = {
-  USERNAME: 'username',
-  PASSWORD: 'password',
-};
-
-const ERROR_MESSAGES = {
-  USERNAME: 'Введите имя',
-  PASSWORD: 'Введите пароль',
-};
-
 export function LoginForm() {
-  const [data, setData] = useState();
+  const handleSubmitData = async event => {
+    event.preventDefault();
 
-  const handleSubmitData = async () => {
-    const data = await baseApi('users', API_METHODS.GET);
-  };
+    const formData = new FormData(event.target);
+    const dataToSend = {
+      email: formData.get('email'),
+      password: formData.get('password'),
+    };
+    const data = await baseApi('auth', API_METHODS.POST, dataToSend);
 
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [errorMessage, setErrorMessage] = useState('');
-  const [isSubmitted, setIsSubmitted] = useState(false);
+    const setCookie = (name, value, days) => {
+      let expires = '';
+      if (days) {
+        let date = new Date();
+        date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000); // setting current day
+        expires = '; expires=' + date.toUTCString();
+      }
+      document.cookie = name + '=' + (value || '') + expires + '; path=/';
+    };
 
-  const handleChange = inputName => value => {
-    if (inputName === INPUT_TYPES.USERNAME) setUsername(value.trim());
-    if (inputName === INPUT_TYPES.PASSWORD) setPassword(value.trim());
+    setCookie('accessToken', data.accessToken, 7); // Set cookie for 7 days
   };
 
   const renderForm = () => (
     <>
-      {data && data.map(item => console.log(item.firstName, '-', item.email))}
-
       <Card width="20%">
         <Flexbox direction="column" gap="8px">
-          <form
-            onSubmit={e => {
-              e.preventDefault();
-              handleSubmitData();
-            }}
-          >
+          <form onSubmit={handleSubmitData}>
             <Text fontSize={sizes.xLarge}>Вход</Text>
             <Flexbox direction="column">
+              <Input name="email" placeholder="Ваше имя" type="text" required />
               <Input
-                placeholder="Ваше имя"
-                onChange={handleChange(INPUT_TYPES.USERNAME)}
-                value={username}
-                type="text"
-                required
-              />
-              {errorMessage}
-              <Input
+                name="password"
                 placeholder="Ваш пароль"
-                onChange={handleChange(INPUT_TYPES.PASSWORD)}
-                value={password}
                 type="password"
                 required
               />
-              {errorMessage}
               <Button type="submit" text="Отправить" />
             </Flexbox>
           </form>
@@ -77,5 +55,5 @@ export function LoginForm() {
       </Card>
     </>
   );
-  return isSubmitted ? <>User is successfully logged in</> : renderForm();
+  return renderForm();
 }
