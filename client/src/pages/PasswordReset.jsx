90b089b5fc67b7664f45cfa171b181ee0c15d770
bashkanitto <aidyn.meiramkhanulu@gmail.com>
2023-12-28 13:31:50 +0600

    fixed passwordReset compomnent

--- client/src/pages/PasswordReset.jsx
+++ client/src/pages/PasswordReset.jsx
@@ -1,42 +1,51 @@
-import React from "react";
-import styled from "styled-components";
+import React from 'react';
+import styled from 'styled-components';
 
-import { Card, Button, Text, Input, Flexbox } from '@atoms';
-import { sizes, colors } from '@base';
+import { Button, Card, Flexbox, Input, Text } from '@atoms';
+import { colors, sizes } from '@base';
 
 const ResetPasswordWrapper = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
   height: 100vh;
-  background-image: linear-gradient(to right, #DEEBFE , #B3CCF2);
+  background-image: linear-gradient(to right, #deebfe, #b3ccf2);
 `;
 
-
-
-
-const PasswordReset = () => {
+export const PasswordReset = () => {
   return (
     <ResetPasswordWrapper>
       <Card width="400px">
         <Flexbox direction="column" gap="12px" align="flex-start">
-          <Text fontSize={sizes.xLarge} weight="bold">Восстановить пароль</Text>
+          <Text fontSize={sizes.xLarge} weight="bold">
+            Восстановить пароль
+          </Text>
           <Flexbox direction="column">
             <Text fontSize={sizes.small}>
               На Ваш e-mail будет выслана ссылка для восстановления пароля
             </Text>
           </Flexbox>
-          <Flexbox direction="column" width='100%' >
-            <Input placeholder="Ваш e-mail" type="email" width='95%' height='48px'/>
-            <Button type="submit" fontSize='23px' text="Восстановить пароль" bgColor={colors.btnPrimary}/>
+          <Flexbox direction="column" width="100%">
+            <Input
+              placeholder="Ваш e-mail"
+              type="email"
+              width="95%"
+              height="48px"
+            />
+            <Button
+              type="submit"
+              fontSize="23px"
+              text="Восстановить пароль"
+              bgColor={colors.btnPrimary}
+            />
           </Flexbox>
-          <Flexbox justify='flex-end' width='100%'>
-            <a style={{textDecoration:'none'}} href="/login"><Text fontSize={sizes.small} >Вернуться на страницу входа</Text></a>
+          <Flexbox justify="flex-end" width="100%">
+            <a style={{ textDecoration: 'none' }} href="/login">
+              <Text fontSize={sizes.small}>Вернуться на страницу входа</Text>
+            </a>
           </Flexbox>
         </Flexbox>
       </Card>
     </ResetPasswordWrapper>
   );
 };
-
-export default PasswordReset;
