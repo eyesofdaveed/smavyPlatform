1a5bf4e5ed20d51e1afff99ce96b062f78c14a95
bashkanitto <aidyn.meiramkhanulu@gmail.com>
2023-12-28 13:31:03 +0600

    fixed card component

--- client/src/atoms/Card.jsx
+++ client/src/atoms/Card.jsx
@@ -4,13 +4,47 @@ import styled from 'styled-components';
 import { colors } from '@base';
 
 export const CardContainer = styled.div`
-  background-color: ${({ bgColor }) => (bgColor ? bgColor : `${colors.cardBg}`)};
-  border-radius: ${({ borderRadius }) => (borderRadius ? borderRadius : '30px')};
-  padding: ${({ padding }) => (padding ? padding : '24px 16px')};
-  display:${({ display }) => (display ? display : 'inline-block')};
+  background-color: ${({ bgColor }) =>
+    bgColor ? bgColor : `${colors.cardBg}`};
+  border-radius: ${({ borderRadius }) =>
+    borderRadius ? borderRadius : '30px'};
+  padding: ${({ padding }) => (padding ? padding : '10px 10px')};
+  justify-content: ${({ justify }) => (justify ? justify : 'flex-start')};
+  align-items: ${({ align }) => (align ? align : 'flex-start')};
+  flex-direction: ${({ direction }) => (direction ? direction : 'row')};
+  display: ${({ display }) => (display ? display : 'inline-block')};
   width: ${({ width }) => (width ? width : '100%')};
+
+  @media screen and (max-width: 450px) {
+    width: 80%;
+  }
 `;
 
-export const Card = ({ width,  children , color, borderRadius, padding, display, bgColor}) => {
-  return <CardContainer width={width} color={color} borderRadius={borderRadius} padding={padding} bgColor={bgColor} display={display} >{children}</CardContainer>;
+export const Card = ({
+  width,
+  children,
+  color,
+  borderRadius,
+  padding,
+  display,
+  direction,
+  justify,
+  bgColor,
+  align,
+}) => {
+  return (
+    <CardContainer
+      width={width}
+      color={color}
+      borderRadius={borderRadius}
+      padding={padding}
+      bgColor={bgColor}
+      display={display}
+      direction={direction}
+      justify={justify}
+      align={align}
+    >
+      {children}
+    </CardContainer>
+  );
 };
