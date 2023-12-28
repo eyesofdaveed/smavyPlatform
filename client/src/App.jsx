b8474626500bc599e64821cc2c2fa255564c17df
eyesofdaveed <davdauit@gmail.com>
2023-12-28 19:26:25 +0600

    user slice with authorization logic

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,15 @@
 import React, { useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import { Public } from './routes/PublicRoutes';
 import { Protected } from './routes/ProtectedRoutes';
-import { decrement, increment } from './store/counterSlice'
 
 const App = () => {
-  const [isAuthorized] = useState(false);
-  const count = useSelector((state) => state.counter.value)
-  const dispatch = useDispatch()
+  const isAuthorized = useSelector(state => state.auth.isAuthorized);
 
   return (
-    <BrowserRouter>
-    <>
-      <button
-            aria-label="Increment value"
-            onClick={() => dispatch(increment())}
-        >
-            Increment
-        </button>
-        <span>{count}</span>
-        <button
-            aria-label="Decrement value"
-            onClick={() => dispatch(decrement())}
-        >
-            Decrement
-        </button>
-    </>
-      {isAuthorized ? <Protected /> : <Public />}
-    </BrowserRouter>
-  );   
+    <BrowserRouter>{isAuthorized ? <Protected /> : <Public />}</BrowserRouter>
+  );
 };
 
 export default App;
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,18 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { Provider } from 'react-redux'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 
-import App from './App.jsx'
-import { store } from './store'
-import './index.css'
-
-import { useSelector, useDispatch } from 'react-redux'
-import { decrement, increment } from './store/counterSlice.js'
+import App from './App.jsx';
+import { store } from './store';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <React.StrictMode>
       <App />
     </React.StrictMode>
-  </Provider>
-)
+  </Provider>,
+);
