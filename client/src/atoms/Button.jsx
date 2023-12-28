2b7786a47ecff3e63999032bfb237751dc8228b9
bashkanitto <aidyn.meiramkhanulu@gmail.com>
2023-12-28 14:01:25 +0600

    fixed btn

--- client/src/atoms/Button.jsx
+++ client/src/atoms/Button.jsx
@@ -1,27 +1,44 @@
 import React from 'react';
 import styled from 'styled-components';
 
-
 export const StyledButton = styled.button`
-  background-color:${({ bgColor }) => (bgColor ? bgColor : '#fff')};
+  background-color: ${({ bgColor }) => (bgColor ? bgColor : '#fff')};
   border-radius: 12px;
-  color:${({color}) => color ? color : '#fff'};
+  color: ${({ color }) => (color ? color : '#fff')};
   outline: none;
   border: none;
   padding: 12px 16px;
-  font-size: ${({fontSize}) => fontSize ? fontSize : '16px'};
+  transition: 0.2s;
+  font-size: ${({ fontSize }) => (fontSize ? fontSize : '16px')};
 
   &:hover {
     cursor: pointer;
     background-color: #f2f2f2;
+    transition: 0.2s;
+    color: black;
   }
   width: ${({ width }) => (width ? width : '100%')};
   color: ${({ color }) => (color ? color : 'white')};
 `;
 
-export const Button = ({ type, text, onClick, bgColor, width, color, fontSize}) => {
+export const Button = ({
+  type,
+  text,
+  onClick,
+  bgColor,
+  width,
+  color,
+  fontSize,
+}) => {
   return (
-    <StyledButton type={type} onClick={onClick} bgColor={bgColor} width={width} color={color} fontSize={fontSize} >
+    <StyledButton
+      type={type}
+      onClick={onClick}
+      bgColor={bgColor}
+      width={width}
+      color={color}
+      fontSize={fontSize}
+    >
       {text}
     </StyledButton>
   );
