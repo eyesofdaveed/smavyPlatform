2e17644272d1070685d1c27b20386488bd9f9f5f
asseemm <assem.shimbergenova@jihc.kz>
2023-12-28 18:58:20 +0600

    fe-redux

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
 
 import App from './App.jsx'
+import { store } from './store'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-      <App />
-  </React.StrictMode>,
-)
+import { useSelector, useDispatch } from 'react-redux'
+import { decrement, increment } from './store/counterSlice.js'
 
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>
+)
