8b587de38259a2e643acdfcd618cdf477d5f5e4f
jqmbylqyzy <dina.zhambylqyzy@jihc.kz>
2023-12-28 18:19:20 +0600

    redux

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-
-import App from './App.jsx'
+import ReactDOM from 'react-dom'
 import './index.css'
+import App from './App'
+import { store } from './store'
+import { Provider } from 'react-redux'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-      <App />
-  </React.StrictMode>,
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+    <Counter />
+  </Provider>,
+  document.getElementById('root')
 )
-
