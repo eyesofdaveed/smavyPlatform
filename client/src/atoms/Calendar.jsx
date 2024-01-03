3be14c217ba0d9299eb2d5c8cfc826fc7a80fee1
akenaaa <aruzhan.tolegen@jihc.kz>
2024-01-03 09:19:53 +0600

    FE-antd-calendar

---
+++ client/src/atoms/Calendar.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Calendar, theme } from 'antd';
+import ruRU from 'antd/es/locale/ru_RU'; // Импортируем русскую локаль
+
+const onPanelChange = (value, mode) => {
+  console.log(value.format('YYYY-MM-DD'), mode);
+};
+
+export const Calendar = () => {
+  const { token } = theme.useToken();
+  const wrapperStyle = {
+    width: 300,
+    border: `1px solid ${token.colorBorderSecondary}`,
+    borderRadius: token.borderRadiusLG,
+  };
+
+  return (
+    <div style={wrapperStyle}>
+      <Calendar fullscreen={false} onPanelChange={onPanelChange} locale={ruRU} /> {/* Устанавливаем русскую локаль */}
+    </div>
+  );
+};
+
