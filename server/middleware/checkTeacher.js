5707eb8974f80939bcfe9eb86f858b92ff97b035
Daulet Zhanysbay <daulet.zhanysbay@gmail.com>
2024-01-13 15:56:36 +0600

    add _id to token

--- server/middleware/checkTeacher.js
+++ server/middleware/checkTeacher.js
@@ -4,7 +4,7 @@ const { ROLES } = require('../enums');
  * @DESC Check Role Middleware
  */
 const checkTeacher = (req, res, next) => {
-    let role = req.user.role;
+    const role = req.user.role;
     if (role !== ROLES.ADMIN || role !== ROLES.TEACHER) {
         return res.status(403).json('Forbidden');
     }
